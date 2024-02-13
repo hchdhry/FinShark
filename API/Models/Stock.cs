8525b9be6eb5bc581b73334349311103b393ca16
hassan <hchoudhury20@hotmail.com>
2024-02-13 14:58:59 +0000

    add models

---
+++ API/Models/Stock.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace API.Models;
+
+public class Stock
+{
+    public int Id { get; set; }
+    
+    public string Symbol { get; set; } = string.Empty;
+
+    public string CompanyName {get;set;} = string.Empty;
+    [Column(TypeName ="decimal(18,2)")]
+    public decimal LastDiv {get;set;}
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal Purchase { get; set; }
+
+    public string Industry { get; set; } = string.Empty;
+
+    public long MarkeyCap {get;set;}
+
+
+}
