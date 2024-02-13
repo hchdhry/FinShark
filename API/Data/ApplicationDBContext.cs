3f299881f6c7d3226092eada91341d8715d16ed9
hassan <hchoudhury20@hotmail.com>
2024-02-13 15:34:02 +0000

    configure Database

---
+++ API/Data/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using API.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace API.Data;
+
+public class ApplicationDBContext:DbContext
+{
+
+public ApplicationDBContext(DbContextOptions dbContextOptions):base(dbContextOptions)
+{
+    
+
+}
+public DbSet<Stock> Stock {get;set;}
+public DbSet<Comment> Comments {get;set;}
+
+}
