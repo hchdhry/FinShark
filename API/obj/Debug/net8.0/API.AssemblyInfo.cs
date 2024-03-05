cfbe38718d3a750382718782ef9d062e75389e44
hassan <hchoudhury20@hotmail.com>
2024-03-05 12:50:58 +0000

    clone front end

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1180759d798fdc747323ef42cbfaa5aa4d5391a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fd8b4b4ddb7e0913d62ebcb7b14eb756efb860c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
